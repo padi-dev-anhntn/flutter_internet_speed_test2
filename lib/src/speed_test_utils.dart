5e0bd9ed0392b99e6c04586b0d8faa276d3cb6d1
WaqasAliAnjum-ZeePalm <waqasali@zeepalm.com>
2024-10-24 15:39:27 +0500

    Package Updated

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -3,7 +3,11 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 Future<bool> isInternetAvailable() async {
   final connectivity = Connectivity();
   final connectivityResult = await connectivity.checkConnectivity();
-  return connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi || 
-      connectivityResult == ConnectivityResult.ethernet;
+  if (connectivityResult.isEmpty) {
+    return false;
+  }
+  var result = connectivityResult.first;
+  return result == ConnectivityResult.mobile ||
+      result == ConnectivityResult.wifi ||
+      result == ConnectivityResult.ethernet;
 }
